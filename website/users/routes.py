cb210f9f5c3b16247abb92bc27403511985e89e5
Virgil Hoover <virgilhoover@gmail.com>
2021-11-22 00:36:34 -0700

    moved default image to exclude the user picture folder

--- website/users/routes.py
+++ website/users/routes.py
@@ -1,84 +1,97 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from website.models import User
-from website.users.forms import LoginForm, RegistrationForm, ResetPasswordForm, RequestResetForm, UpdateAccountForm
+from website.users.forms import (
+    LoginForm,
+    RegistrationForm,
+    ResetPasswordForm,
+    RequestResetForm,
+    UpdateAccountForm,
+)
 from website.users.utils import send_reset_email, save_picture
 from website import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 from datetime import timedelta
 
-users = Blueprint('users', __name__)
+users = Blueprint("users", __name__)
 
 
-@users.route('/login', methods=['GET', 'POST'])
+@users.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('ideas.home'))
+        return redirect(url_for("ideas.home"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data, duration=timedelta(hours=24))
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('ideas.home'))
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("ideas.home"))
         else:
-            flash('Login Unsuccessful. Please check email and password', 'error')
-    return render_template('login.html', title='Login', form=form, user=current_user)
+            flash("Login Unsuccessful. Please check email and password", "error")
+    return render_template("login.html", title="Login", form=form, user=current_user)
 
 
-@users.route('/logout')
+@users.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('users.login'))
+    return redirect(url_for("users.login"))
 
 
-@users.route('/sign_up', methods=['GET', 'POST'])
+@users.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
     if current_user.is_authenticated:
-        return redirect(url_for('ideas.home'))
+        return redirect(url_for("ideas.home"))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(first_name=form.first_name.data, email=form.email.data,
-                    password=generate_password_hash(form.password.data, method='sha256'))
+        user = User(
+            first_name=form.first_name.data,
+            email=form.email.data,
+            password=generate_password_hash(form.password.data, method="sha256"),
+        )
         db.session.add(user)
         db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('users.login'))
-    return render_template('sign_up.html', title='Register', form=form, user=current_user)
+        flash("Your account has been created! You are now able to log in", "success")
+        return redirect(url_for("users.login"))
+    return render_template(
+        "sign_up.html", title="Register", form=form, user=current_user
+    )
 
 
-@users.route('/reset_password', methods=['GET', 'POST'])
+@users.route("/reset_password", methods=["GET", "POST"])
 def reset_request():
     if current_user.is_authenticated:
-        return redirect(url_for('ideas.home'))
+        return redirect(url_for("ideas.home"))
     form = RequestResetForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         send_reset_email(user)
-        flash('An email has been sent with instructions to reset your password', 'success')
-        return redirect(url_for('users.login'))
-    return render_template('reset_request.html', title='Reset Password', form=form)
+        flash(
+            "An email has been sent with instructions to reset your password", "success"
+        )
+        return redirect(url_for("users.login"))
+    return render_template("reset_request.html", title="Reset Password", form=form)
 
 
-@users.route('/reset_password/<token>', methods=['GET', 'POST'])
+@users.route("/reset_password/<token>", methods=["GET", "POST"])
 def reset_token(token):
     if current_user.is_authenticated:
-        return redirect(url_for('ideas.home'))
+        return redirect(url_for("ideas.home"))
     user = User.verify_reset_token(token)
     if user is None:
-        flash('That token is invalid or expired.', 'warning')
-        return redirect(url_for('users.reset_request'))
+        flash("That token is invalid or expired.", "warning")
+        return redirect(url_for("users.reset_request"))
     form = ResetPasswordForm()
     if form.validate_on_submit():
-        user.password = generate_password_hash(form.password.data, method='sha256')
+        user.password = generate_password_hash(form.password.data, method="sha256")
         db.session.commit()
-        flash('Your password has been updated! You are now able to log in', 'success')
-        return redirect(url_for('users.login'))
-    return render_template('reset_token.html', title='Rest Password', form=form)
+        flash("Your password has been updated! You are now able to log in", "success")
+        return redirect(url_for("users.login"))
+    return render_template("reset_token.html", title="Rest Password", form=form)
 
 
-@users.route("/account", methods=['GET', 'POST'])
+@users.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
     form = UpdateAccountForm()
@@ -89,10 +102,12 @@ def account():
         current_user.first_name = form.first_name.data
         current_user.email = form.email.data
         db.session.commit()
-        flash('Your account has been updated!', 'success')
-        return redirect(url_for('users.account'))
-    elif request.method == 'GET':
+        flash("Your account has been updated!", "success")
+        return redirect(url_for("users.account"))
+    elif request.method == "GET":
         form.first_name.data = current_user.first_name
         form.email.data = current_user.email
-    img_file = url_for('static', filename='/pics/' + current_user.img_file)
-    return render_template('account.html', title='Account', image_file=img_file, form=form)
+    img_file = url_for("static", filename="/" + current_user.img_file)
+    return render_template(
+        "account.html", title="Account", image_file=img_file, form=form
+    )
