1984b1daca533f74856482f485a904ca616f5ac8
Virgil Hoover <virgilhoover@gmail.com>
2021-11-28 04:44:52 -0700

    added platform dependant location for config file

--- website/config.py
+++ website/config.py
@@ -1,17 +1,23 @@
 from json import load as jload
+from sys import platform
 
-with open('/etc/config.json') as config_file:
+if platform == "win32":
+    file = "c:\\config.json"
+else:
+    file = "/etc/config.json"
+
+with open(file) as config_file:
     config = jload(config_file)
 
 
 class Config:
-    SECRET_KEY = config.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = "smtp.gmail.com"
     MAIL_Port = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = config.get('MAIL_USER')
-    MAIL_PASSWORD = config.get('MAIL_PASSWORD')
-    MAIL_RECIPIENT = config.get('MAIL_RECIPIENT')
+    MAIL_USERNAME = config.get("MAIL_USER")
+    MAIL_PASSWORD = config.get("MAIL_PASSWORD")
+    MAIL_RECIPIENT = config.get("MAIL_RECIPIENT")
